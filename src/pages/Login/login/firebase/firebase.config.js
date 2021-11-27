31792a1da6d5df94b6e293d407f08a843d245213
krishnaCheashty <krishnacheashty@gmail.com>
2021-11-28 00:20:35 +0600

    .env process

--- src/pages/Login/login/firebase/firebase.config.js
+++ src/pages/Login/login/firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCGoil-D8YKXmlSMbVV-hpSSqeOTCfuyRY",
-    authDomain: "diamond-house-1024c.firebaseapp.com",
-    projectId: "diamond-house-1024c",
-    storageBucket: "diamond-house-1024c.appspot.com",
-    messagingSenderId: "600105304817",
-    appId: "1:600105304817:web:31c7b9d208f2ba63a19f50"
+    apiKey:process.env.REACT_APP_API_KEY,
+    authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId:process.env.REACT_APP_APP_ID,
   };
 
 export default firebaseConfig;
